13383c295fdd818bb18bc21d8821f88231ebd1ac
deveyy <108226797+deveyy@users.noreply.github.com>
2022-10-06 11:42:50 +0700

    feat: implement project (#2)
    * config server app
    
    * connecting db mongosee db
    
    * fix bug connect db
    
    * install bunyan and config logger
    
    * add readme
    
    Co-authored-by: @deveyy  <jungjihoonkr97@gmail.com>

--- app.js
+++ app.js
@@ -5,18 +5,48 @@
  * @email thiendinh.dev@gmail.com
  */
 
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
+// mongose
+const mongose = require('mongoose');
+
+// config bunyan
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({
+    name: 'app-bdigital',
+    serializers: bunyan.stdSerializers,
+    level: "debug"
+});
+
+// app
 const app = express();
 
-require('dotenv').config()
+//db
+const URL_DB = process.env.DATABASE;
 
+try {
+    mongose.connect(URL_DB, {
+        useNewUrlParser: true,
+        // bug MongoParseError: option usecreateindex is not supported is ver mongosee > 6
+       // useCreateIndex: true
+        useUnifiedTopology: true,
+    }).then(() => {
+        log.info('Database Connected To MongoDB!!');
+    })
+} catch (error) {
+        log.info(error, "Server error, try again");
+}
+
+
+
+//routes
 app.get('/', (req, res) => {
     res.send('Hi Bdigital Bot Server start')
 })
 
-const port = process.env.PRORT || 8000
+const port = process.env.PORT || 8000
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    log.info(`Server is running on port ${port}`);
 });
