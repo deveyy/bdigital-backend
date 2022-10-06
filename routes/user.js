3a98ba9ae2b2659b61d22587d542985ca1c0511b
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-06 11:58:54 +0700

    config controller and routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi Bdigital Bot Server start');
-});
+const {sayHi} =  require("../controllers/user");
+
+router.get('/', sayHi);
+
+module.exports = router;
