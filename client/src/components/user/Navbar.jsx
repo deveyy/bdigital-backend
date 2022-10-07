3177734158f3edd2630674128f6d478775d8ea2b
deveyy <108226797+deveyy@users.noreply.github.com>
2022-10-07 19:27:13 +0700

    feat: implement components client (#9)
    * Initializing Project and Tailwind css
    
    * Create User Navbar
    
    * add react-icons
    
    * add components
    
    * update
    
    Co-authored-by: @deveyy  <jungjihoonkr97@gmail.com>

---
+++ client/src/components/user/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Logo from '../img/logo.png';
+import { BsFillSunFill } from 'react-icons/bs'
+import Container from "../Container"
+
+
+export default function Navbar() {
+    return (
+      <div className="bg-secondary drop-shadow-sm shadow-gray-50">
+            <Container className="max-w-screen-lg mx-auto p-2">
+                <div className="flex justify-between items-center">
+                    <img src={Logo}  alt="logo" className="h-10"/>
+                    <ul>
+                        <li>
+                            <button className="bg-dark-subtle p-1 rounded">
+                                <BsFillSunFill className="text-secondary" size={24} />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </Container>
+      </div>
+    )
+}
