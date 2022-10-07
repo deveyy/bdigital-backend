511ec39b98f74fe6ca0fcb3c473a010bdd80ef71
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-07 21:18:08 +0700

    forget password form

--- client/src/components/auth/ForgetPassword.jsx
+++ client/src/components/auth/ForgetPassword.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Container from '../Container'
+import FormInput from '../form/FormInput'
+import Submit from '../form/Submit'
+import Title from '../form/Title'
+import CustomLink from '../link/CustomLink'
 
 export default function ForgetPassword() {
   return (
-    <div>ForgetPassword</div>
+    <div className='fixed inset-0 bg-slate-200 -z-10 flex justify-center items-center'>
+        <Container>
+            <form className='bg-slate-500 rounded p-6 w-72'>
+                <Title>Forget Password</Title>
+                <FormInput label="Email" type="email" name="email" placeholder="Please input email" />
+                <Submit value="Send" />
+
+                <div className="mt-2 flex justify-between">
+                  <CustomLink to="/admin/auth/signin">Sign in</CustomLink>
+                  <CustomLink to="/admin/auth/signup">Sign up</CustomLink>
+               </div>
+            </form>
+        </Container>
+    </div>
   )
 }
