de289cc19ef6dec4b224ff7772043081ee95d8a6
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-07 17:52:58 +0700

    add react-icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-
 import Logo from './logo.png'
+import { BsFillSunFill } from 'react-icons/bs'
+
 
 export default function Navbar() {
     return (
@@ -8,6 +9,13 @@ export default function Navbar() {
             <div className="max-w-screen-lg mx-auto p-2">
                 <div className="flex justify-between items-center">
                     <img src={Logo}  alt="logo" className="h-10"/>
+                    <ul>
+                        <li>
+                            <button className="bg-dark-subtle p-1 rounded">
+                                <BsFillSunFill className="text-secondary" size={24} />
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
       </div>
