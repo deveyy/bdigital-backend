930a23ebd56dce972e68e8d3f4c951774fa5a5c8
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-08 11:25:50 +0700

    signup state (userInfo)

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { commonModalClasses } from '../../utils/theme';
 import Container from '../Container';
 import FormContainer from '../form/FormContainer';
@@ -8,14 +8,49 @@ import Title from '../form/Title';
 import CustomLink from '../link/CustomLink';
 
 export default function Signup() {
+
+  const [ userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setUserInfo({...userInfo, [name]: value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const { name, email, password} = userInfo;
+
   return (
     <FormContainer>
         <Container>
-            <form className={commonModalClasses + "w-72"}>
+            <form onSubmit={handleSubmit}  className={commonModalClasses + "w-72"}>
                 <Title>Sign up</Title>
-                <FormInput label="Name" name="name" placeholder="Please input name" />
-                <FormInput label="Email" type="email" name="email" placeholder="Please input email" />
-                <FormInput label="Password" type="password" name="password" placeholder="Please input password" />
+                <FormInput 
+                  value={name} 
+                  onChange={handleChange}
+                  label="Name" 
+                  name="name" 
+                  placeholder="Please input name" />
+                <FormInput 
+                  value={email}
+                  onChange={handleChange} 
+                  label="Email" 
+                  type="email" 
+                  name="email" 
+                  placeholder="Please input email" />
+                <FormInput 
+                  value={password}
+                  onChange={handleChange} 
+                  label="Password" 
+                  type="password" 
+                  name="password" 
+                  placeholder="Please input password" />
                 <Submit value="Sign up" />
 
                 <div className="mt-2 flex justify-between">
