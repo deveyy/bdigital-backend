a0f3ef3191b827f357cae3c5e57fab3d66fcb369
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-08 10:13:20 +0700

    confirm password form

--- client/src/components/auth/ConfirmPassword.jsx
+++ client/src/components/auth/ConfirmPassword.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Container from '../Container';
+import FormInput from '../form/FormInput';
+import Submit from '../form/Submit';
+import Title from '../form/Title';
 
 export default function ConfirmPassword() {
   return (
-    <div>ConfirmPassword</div>
+    <div className='fixed inset-0 bg-slate-200 -z-10 flex justify-center items-center'>
+    <Container>
+        <form className='bg-slate-500 rounded p-6 w-72'>
+            <Title>Enter New Password</Title>
+            <FormInput label="New Password" type="password" name="password" placeholder="Please input password" />
+            <FormInput label="Confirm Password" type="password" name="confirmPassword" placeholder="Please input confirm password" />
+            <Submit value="Submit" />
+        </form>
+    </Container>
+</div>
   );
 }
