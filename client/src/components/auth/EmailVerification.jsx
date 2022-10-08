613cbdeb7e8c6d4df0583e41ec564056c3e45c3b
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-08 10:05:52 +0700

    Previous from empty OTP

--- client/src/components/auth/EmailVerification.jsx
+++ client/src/components/auth/EmailVerification.jsx
@@ -14,13 +14,14 @@ export default function EmailVerification() {
 
     const focusNextInputField = (index) => {
         setActiveOtpIndex(index + 1);
-    }
+    };
+
     const focusPrevInputField = (index) => {
         let nextIndex;
         const diff = index - 1;
         nextIndex = diff !== 0 ? diff : 0;
         setActiveOtpIndex(nextIndex);
-    }
+    };
 
     const handleOtpChange = ({target}, index) => {
         const {value} = target;
@@ -28,13 +29,19 @@ export default function EmailVerification() {
         newOtp[index] = value.substring(value.length -1, value.length);
         
         if(!value) {
-            focusPrevInputField(index)
+            focusPrevInputField(index);
         } else {
             
             focusNextInputField(index);
         }
         setOtp([...newOtp]);
 
+    };
+
+    const handleKeyDown = ({key}, index) => {
+        if(key === "Backspace") {
+            focusPrevInputField(index);
+        }
     };
 
     const inputRef = useRef();
@@ -61,6 +68,7 @@ export default function EmailVerification() {
                         key={index}
                         value={otp[index] || ''}
                         onChange={(e) => handleOtpChange(e, index)}
+                        onKeyDown={(e) => handleKeyDown(e, index)}
                         type='number' 
                         className="w-10 h-10 border-2 dark:border-dark-subtle
                       border-light-subtle dark:focus:border-white
