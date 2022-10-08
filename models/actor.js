84efa542deae06d328455ba4799f1bc9cc321638
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-08 17:44:40 +0700

    Actor schema

---
+++ models/actor.js
@@ -0,0 +1,38 @@
+/**
+ * @author ddthien.dev
+ * @date 2022-10-08
+ * @contact
+ * @email thiendinh.dev@gmail.com
+ */
+
+ import mongoose from "mongoose";
+
+const actorSchema = mongoose.Schema(
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      about: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      gender: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      avatar: {
+        type: Object,
+        url: String,
+        public_id: String,
+      },
+    },
+    { timestamps: true }
+  );
+  
+  actorSchema.index({ name: "text" });
+
+  export default mongoose.model("Actor", actorSchema);
