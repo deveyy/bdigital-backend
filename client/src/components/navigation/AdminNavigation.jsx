b202a6cd6d518f4a857c23bf5dc971b86722f08c
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-10 08:20:59 +0700

    make admin navbar visible

--- client/src/components/navigation/AdminNavigation.jsx
+++ client/src/components/navigation/AdminNavigation.jsx
@@ -3,15 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Actors from "../admin/Actors";
 import Dashboard from "../admin/Dashboard";
 import Movies from "../admin/Movies";
+import Navbar from "../admin/Navbar";
 import NotFound from "../NotFound";
 
 export default function AdminNavigation() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/actors" element={<Actors />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="flex">
+      <Navbar />
+      <div className="flex-1 p-2 max-w-screen-lg">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
